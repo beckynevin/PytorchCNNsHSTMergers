2172f74a88c5dbefa1cc00d9a0c2bc3e829dc2b9
Aimee Schechter <aimee.schechter@colorado.edu>
2023-04-13 19:07:37 -0400

    saving accuracy

--- CreateDataClass.py
+++ CreateDataClass.py
@@ -26,21 +26,21 @@ class BinaryMergerDataset(Dataset):
                 self.img_labels = np.load(data_path + 'training/anymergers/mergerlabel.npy')
             else:
                 self.images = glob.glob(data_path + 'training/nonmergers/allfilters*.npy')
-                self.img_labels = data_path + 'training/nonmergers/mergerlabel.npy'
+                self.img_labels = np.load(data_path + 'training/nonmergers/mergerlabel.npy')
         elif self.dataset == 'validation':
             if mergers == True:
                 self.images = glob.glob(data_path + 'validation/anymergers/allfilters*.npy')
-                self.img_labels = glob.glob(data_path + 'validation/anymergers/mergerlabel.npy')
+                self.img_labels = np.load(data_path + 'validation/anymergers/mergerlabel.npy')
             else:
                 self.images = glob.glob(data_path + 'validation/nonmergers/allfilters*.npy')
-                self.img_labels = glob.glob(data_path + 'validation/nonmergers/mergerlabel.npy')
+                self.img_labels = np.load(data_path + 'validation/nonmergers/mergerlabel.npy')
         elif self.dataset == 'test':
             if mergers == True:
                 self.images = glob.glob(data_path + 'test/anymergers/allfilters*.npy')
-                self.img_labels = glob.glob(data_path + 'test/anymergers/mergerlabel.npy')
+                self.img_labels = np.load(data_path + 'test/anymergers/mergerlabel.npy')
             else:
                 self.images = glob.glob(data_path + 'test/nonmergers/allfilters*.npy')
-                self.img_labels = glob.glob(data_path + 'test/nonmergers/mergerlabel.npy')
+                self.img_labels = np.load(data_path + 'test/nonmergers/mergerlabel.npy')
         
         self.transform = transform
         
@@ -51,14 +51,14 @@ class BinaryMergerDataset(Dataset):
     def __getitem__(self, idx):
         img_path = self.images[idx]
         image = np.load(img_path) #keep as np array to normalize
-        image = image[:,:,0:3]
+        image = image[:,:,1:4]
         #print('image shape: ', np.shape(image))
         image = stretch(image)
         label_file = self.img_labels
         #print('first label call: ', np.shape(label))
         label = label_file[idx]
-        if label != 0:
-            print(label)
+        # if label != 0:
+        #     print(label)
         #print(labels)
         #label = np.load(label_path)[idx]
         #print('label shape: ',np.shape(labels))
@@ -81,50 +81,60 @@ def get_transforms(train=True):
         T.RandomRotation(30), 
         T.RandomHorizontalFlip(0.5),
         T.RandomVerticalFlip(0.5),
-        T.Pad(22)
+        T.Pad(11)
         ))
-    else: transforms.append(T.Pad(22))
+    else: transforms.append(T.Pad(11))
         
     return T.Compose(transforms)
+
+def save_checkpoint(model, optimizer, save_path, epoch):
+    torch.save({
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'epoch': epoch
+    }, save_path)
+
+accuracy = np.array([])
 
 train_mergers_dataset = BinaryMergerDataset(path, 'train', mergers = True, transform = get_transforms(train=True))
 train_nonmergers_dataset = BinaryMergerDataset(path, 'train', mergers = False, transform = get_transforms(train=True))
 
 train_dataset_full = torch.utils.data.ConcatDataset([train_mergers_dataset, train_nonmergers_dataset])
-train_dataloader = DataLoader(train_dataset_full, shuffle = True, num_workers = 4, batch_size=32)
+train_dataloader = DataLoader(train_dataset_full, shuffle = True, num_workers = 1, batch_size=32)
 
 validation_mergers_dataset = BinaryMergerDataset(path, 'validation', mergers = True, transform = get_transforms(train=False))
 validation_nonmergers_dataset = BinaryMergerDataset(path, 'validation', mergers = False, transform = get_transforms(train=False))
 
 validation_dataset_full = torch.utils.data.ConcatDataset([validation_mergers_dataset, validation_nonmergers_dataset])
-validation_dataloader = DataLoader(validation_dataset_full, shuffle = True, num_workers = 4, batch_size=32)
+validation_dataloader = DataLoader(validation_dataset_full, shuffle = True, num_workers = 1, batch_size=32)#num workers used to be 4
 
-images, labels = next(iter(train_dataloader)) 
-
+#images, labels = next(iter(train_dataloader)) 
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
-model = models.alexnet(pretrained=True)
-model.classifier[6] = torch.nn.Linear(model.classifier[6].in_features, 2)
+model = models.alexnet(pretrained=True, progress = True)
+#print(model)
+
+model.classifier[6] = torch.nn.Linear(model.classifier[6].in_features, 1)
 NUM_EPOCHS = 10
 BEST_MODEL_PATH = 'best_model.pth'
 best_accuracy = 0.0
 
-optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+optimizer = optim.AdamW(model.parameters(), lr=0.0001)
 
 for epoch in range(NUM_EPOCHS):
     
     for images, labels in iter(train_dataloader):
         images = torch.tensor(images, dtype=torch.float32).to(device)
         labels = torch.tensor(labels, dtype=torch.float32).to(device)
+        #print(images.size())
         optimizer.zero_grad()
         outputs = model(images)
-        print(outputs.size())
-        print(labels.size())
         labels = labels.unsqueeze(1)
-        # labels = labels.float()
-        loss = F.binary_cross_entropy(outputs, labels)
+        #print(outputs.size())
+        #print(labels.size())
+        loss = F.binary_cross_entropy_with_logits(outputs, labels)
         loss.backward()
         optimizer.step()
     
@@ -140,3 +150,7 @@ for epoch in range(NUM_EPOCHS):
     if val_accuracy > best_accuracy:
         torch.save(model.state_dict(), BEST_MODEL_PATH)
         best_accuracy = val_accuracy
+        np.append(accuracy, val_accuracy)
+    save_checkpoint(model=model, optimizer=optimizer, save_path='/n/home09/aschechter/code/BinaryCNNTesting/PytorchCNNs/savedmodel.txt', epoch = epoch)
+print('best accuracy:', best_accuracy)
+np.save('/n/home09/aschechter/code/BinaryCNNTesting/PytorchCNNs/accuracy_transfer.txt', accuracy)
